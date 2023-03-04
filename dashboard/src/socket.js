458e1e09ad1b953e8ce349fc64f465b246539de5
Marcin Włodarczyk <marcin.wlodarczyk98@gmail.com>
2023-03-04 23:35:00 +0100

    Fix of socket io server IP address

--- dashboard/src/socket.js
+++ dashboard/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import config from './config.js'
 
-const URL = "http://127.0.0.1:3000";
+const URL = config.gameApi.url;
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
